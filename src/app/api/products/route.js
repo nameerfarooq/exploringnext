460300d70e8a07a786d9350f65f5f214885c1d7a
Nameerfarooq <nameerfarooquit@gmail.com>
2024-07-19 02:15:34 +0500

    update

--- src/app/api/products/route.js
+++ src/app/api/products/route.js
@@ -1,5 +1,7 @@
 import { NextResponse } from "next/server";
 
-export async function  GET(req) {
+export async function GET(req) {
+  const  searchParams  = req.nextUrl.searchParams;
+  console.log("searchParams search value is : ", searchParams.get("search"));
   return NextResponse.json({ msg: "Hello world learning next" });
 }
