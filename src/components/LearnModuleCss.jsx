bcac8f7645394f07d8fa290da56c5fbb366ed5a4
Nameerfarooq <nameerfarooquit@gmail.com>
2024-07-19 00:53:45 +0500

    Image optimised

--- src/components/LearnModuleCss.jsx
+++ src/components/LearnModuleCss.jsx
@@ -1,8 +1,11 @@
+import Image from 'next/image'
 import styles from '../css/LearnCss.module.css'
+import postman from '../../public/images/postman.webp'
 const LearnModuleCss = () => {
   return (
     <div className={styles.my_green_text}>
       Learn Module Css
+      <Image src={postman} alt='Postman Certificate image' width={"100%"}/>
     </div>
   )
 }
