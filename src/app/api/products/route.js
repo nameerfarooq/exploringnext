4e81018d466add45a254b802bc859b6607358d3e
Nameerfarooq <nameerfarooquit@gmail.com>
2024-07-21 23:58:02 +0500

    POST req in NEXT JS

--- src/app/api/products/route.js
+++ src/app/api/products/route.js
@@ -1,7 +1,13 @@
 import { NextResponse } from "next/server";
 
 export async function GET(req) {
-  const  searchParams  = req.nextUrl.searchParams;
+  const searchParams = req.nextUrl.searchParams;
   console.log("searchParams search value is : ", searchParams.get("search"));
   return NextResponse.json({ msg: "Hello world learning next" });
+}
+
+export async function POST(req) {
+  const res = await req.json();
+  console.log(res);
+  return NextResponse.json({ msg: "Product added" });
 }
